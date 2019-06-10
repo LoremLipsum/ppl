b653d1d1c7d41346e0fa2c6d7328c3b329275a5e
loremlipsum <lipsum.webdevelop@gmail.com>
2019-06-10 07:31:15 +0300

    'lipsum'

--- public/scripts/blocks/dropdown.js
+++ public/scripts/blocks/dropdown.js
@@ -3,6 +3,7 @@
 	$(function() {
     var btn;
     var btnsDropdown = $('.js-dropdown-button');
+    var btnsFooter = $('.js-footer-nav-dropdown-button');
 
     if (btnsDropdown) {
       btnsDropdown.on('click', function(e){
@@ -12,6 +13,23 @@
         btn.parents('.js-dropdown').toggleClass('active');
       });
     }
+
+    var initDropdown = function() {
+      if ($(window).outerWidth() < 620) {
+        btnsFooter.on('click', function(e){
+          e.preventDefault();
+          btn = $(this);
+          btnsFooter.not(btn).parents('.js-footer-nav-dropdown').removeClass('active');
+          btn.parents('.js-footer-nav-dropdown').toggleClass('active');
+        });
+      }
+    }
+
+    initDropdown();
+
+    $(window).resize(function() {
+      initDropdown();
+    });
 
 	});
 })(jQuery);
